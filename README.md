4dad0ef2739c704773bc661c0ddf6f23cb22ba97
Maneesh Tewani <mtewani@google.com>
2024-08-21 13:49:30 -0700

    Updated path export

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 Most options available in the `flutter create` cli.
 
-<a href="https://idx.google.com/new?template=https://github.com/project-idx/community-templates/tree/main/flutter-create">
+<a href="https://idx.google.com/new?template=https://github.com/maneesht/idx-dart-test/tree/main">
   <img height="32" alt="Try in IDX" src="https://cdn.idx.dev/btn/try_dark_32.svg">
 </a>
--- idx-template.nix
+++ idx-template.nix
@@ -9,7 +9,7 @@
     ];
     bootstrap = ''
         export HOME=/home/user
-        PATH="$PATH":"$HOME/.pub-cache/bin"
+        export PATH="$PATH":"$HOME/.pub-cache/bin"
         flutter create "$out" --template="${template}" --platforms="${platforms}" ${if sample == "none" then "" else "--sample=${sample}"} ${if blank then "-e" else ""}
         mkdir "$out"/.idx
         cp ${./dev.nix} "$out"/.idx/dev.nix
